b7b5eebaeb7e53fc54eec5868f3ae684c1a7edb2
RondonLeonR <RondonLeonR@github.com>
2021-07-21 17:01:32 -0300

    Empezando la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/bienvenido', pathMatch: 'full'
+  },
+  {
+    path: 'bienvenido', component: BienvenidoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
